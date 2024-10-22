063174274387d13afdb1398438e2f1248db7034f
juanchehin <chehinjuan@gmail.com>
2024-10-22 18:41:29 -0300

    eventos cancelados tachados

--- silver/src/app/pages/mantenimientos/calendario/citas/citas.component.ts
+++ silver/src/app/pages/mantenimientos/calendario/citas/citas.component.ts
@@ -9,7 +9,7 @@ import { ServiciosService } from 'src/app/services/servicios.service';
 @Component({
   selector: 'app-citas',
   templateUrl: './citas.component.html',
-  styles: []
+  styleUrls: ['./citas.component.css']
 })
 export class CitasComponent implements OnInit {
 
@@ -52,29 +52,29 @@ export class CitasComponent implements OnInit {
   clienteBuscado = '';
 
   horarios = [
-    { label: '08:00 AM' , hora: '08:00:00' },
-    { label: '08:30 AM' , hora: '08:30:00' },
-    { label: '09:00 AM' , hora: '09:00:00' },
-    { label: '09:30 AM' , hora: '09:30:00' },
-    { label: '10:00 AM' , hora: '10:00:00' },
-    { label: '10:30 AM' , hora: '10:30:00' },
-    { label: '11:00 AM' , hora: '11:00:00' },
-    { label: '11:30 AM' , hora: '11:30:00' },
-    { label: '12:00 PM' , hora: '12:00:00' },
-    { label: '12:30 PM' , hora: '12:30:00' },
-    { label: '01:00 PM' , hora: '13:00:00' },
-    { label: '01:30 PM' , hora: '13:30:00' },
-    { label: '02:00 PM' , hora: '14:00:00' },
-    { label: '02:30 PM' , hora: '14:30:00' },
-    { label: '03:00 PM' , hora: '15:00:00' },
-    { label: '03:30 PM' , hora: '15:30:00' },
-    { label: '04:00 PM' , hora: '16:00:00' },
-    { label: '04:30 PM' , hora: '16:30:00' },
-    { label: '05:00 PM' , hora: '17:00:00' },
-    { label: '05:30 PM' , hora: '17:30:00' },
-    { label: '06:00 PM' , hora: '18:00:00' },
-    { label: '06:30 PM' , hora: '18:30:00' },
-    { label: '07:00 PM' , hora: '19:00:00' }
+    { label: '08:00 AM' , hora: '08:00:00' , estado_evento: '' },
+    { label: '08:30 AM' , hora: '08:30:00' , estado_evento: '' },
+    { label: '09:00 AM' , hora: '09:00:00' , estado_evento: '' },
+    { label: '09:30 AM' , hora: '09:30:00' , estado_evento: '' },
+    { label: '10:00 AM' , hora: '10:00:00' , estado_evento: '' },
+    { label: '10:30 AM' , hora: '10:30:00' , estado_evento: '' },
+    { label: '11:00 AM' , hora: '11:00:00' , estado_evento: '' },
+    { label: '11:30 AM' , hora: '11:30:00' , estado_evento: '' },
+    { label: '12:00 PM' , hora: '12:00:00' , estado_evento: '' },
+    { label: '12:30 PM' , hora: '12:30:00' , estado_evento: '' },
+    { label: '01:00 PM' , hora: '13:00:00' , estado_evento: '' },
+    { label: '01:30 PM' , hora: '13:30:00' , estado_evento: '' },
+    { label: '02:00 PM' , hora: '14:00:00' , estado_evento: '' },
+    { label: '02:30 PM' , hora: '14:30:00' , estado_evento: '' },
+    { label: '03:00 PM' , hora: '15:00:00' , estado_evento: '' },
+    { label: '03:30 PM' , hora: '15:30:00' , estado_evento: '' },
+    { label: '04:00 PM' , hora: '16:00:00' , estado_evento: '' },
+    { label: '04:30 PM' , hora: '16:30:00' , estado_evento: '' },
+    { label: '05:00 PM' , hora: '17:00:00' , estado_evento: '' },
+    { label: '05:30 PM' , hora: '17:30:00' , estado_evento: '' },
+    { label: '06:00 PM' , hora: '18:00:00' , estado_evento: '' },
+    { label: '06:30 PM' , hora: '18:30:00' , estado_evento: '' },
+    { label: '07:00 PM' , hora: '19:00:00' , estado_evento: '' }
   ];
   
 
